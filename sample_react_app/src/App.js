199f39b5e01383f3dd370a032c54cdab168f2d18
Hwangtaehun <r283@naver.com>
2025-02-16 19:57:16 +0900

    업로드

---
+++ sample_react_app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+
+function App(props) {
+  const [counter, setCounter] = useState(0);
+  const doClick = ()=> {
+    setCounter(counter+1);
+  }
+  return (
+    <div className="App">
+      <h1 className={props.color}>{props.title}</h1>
+      <p className={props.color,"clickable"} onClick={doClick}>counter: {counter}.</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ sample_react_app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App color="red" title="Hello!" message="이것은 color=red의 표시입니다." />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
