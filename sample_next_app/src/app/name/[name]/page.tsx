b653f5ec1967cd5f6c3b583d58c0c3597098b2c6
Hwangtaehun <r283@naver.com>
2025-02-17 20:35:09 +0900

    업로드

--- sample_next_app/src/app/name/[name]/page.tsx
+++ sample_next_app/src/app/name/[name]/page.tsx
@@ -2,7 +2,7 @@ export default function Name({params}:{params:{name: string}}) {
     return(
         <main>
             <h1 className="title">Name page</h1>
-            <p className="msg">당신은 {params.name} 님이군요.</p>
+            <p className="msg">당신은 '{params.name}' 님이군요.</p>
             <div>
                 <a href="/">go back!!</a>
             </div>
