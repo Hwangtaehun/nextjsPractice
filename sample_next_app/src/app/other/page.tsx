f2b6cad1978ad0a8812f6cbc4b189a31209cdcb9
Hwangtaehun <r283@naver.com>
2025-02-17 20:51:49 +0900

    업로드

--- sample_next_app/src/app/other/page.tsx
+++ sample_next_app/src/app/other/page.tsx
@@ -1,14 +1,25 @@
+'use client'
+
 import Link from "next/link"
 import Image from "next/image"
 import styles from './style.module.css';
+import JSXStyle from "styled-jsx/style";
 
 export default function Other(){
     return (
         <main>
+            <JSXStyle>
+                {`p.jsx-msg{
+                  margin: 10px;
+                  text-align: center;
+                  color: red;
+                  font-weight: bold;
+                }`}
+            </JSXStyle>
             <h1 className={styles.msg}>Other page</h1>
-            <p className={styles.title}>이건 다른 페이지입니다.</p>
+            <p className="jsx-msg">이건 다른 페이지입니다.</p>
             <div>
-                <Image src="/sample.jpg" width={200} height={200} />
+                <Image src="/sample.jpg" width={200} height={200} alt="wait..."/>
             </div>
             <div>
                 <Link href="/">go back!!</Link>
