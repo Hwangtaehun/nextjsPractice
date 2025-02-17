edb10a47226a4beaece45b9735d8f4e844052375
Hwangtaehun <r283@naver.com>
2025-02-17 20:07:06 +0900

    업로드

--- sample_next_app/src/app/page.tsx
+++ sample_next_app/src/app/page.tsx
@@ -1,8 +1,29 @@
+'use client'
+
+import {useState} from 'react';
+
 export default function Home() {
+  var [input, setInput] = useState("");
+  var [message, setMessage] = useState("your name:");
+
+  const doChange = (event)=> {
+    setInput(event.target.value);
+  };
+
+  const doClick = ()=> {
+    setMessage("Hello, " + input + "!!");
+    setInput("");
+  }
+
   return(
     <main>
-      <h1>Next.js sample.</h1>
-      <p>This is sample application.</p>
+      <h1 className="text-2xl m-5">Next.js sample.</h1>
+      <p className="text-lg m-5">{message}</p>
+      <div className="m-5">
+        <input type="text" onChange={doChange} value={input}
+         className="p-1 border-solid border-2 border-gray-400"/>
+        <button onClick={doClick} className="px-7 py-2 mx-2 bg-blue-800 text white rounded-lg">Click</button>
+      </div>
     </main>
   );
 }
