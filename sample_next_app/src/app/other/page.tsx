4839840dbc81c9c41e3df33c8cb598822152d9f8
Hwangtaehun <r283@naver.com>
2025-02-17 20:42:44 +0900

    업로드

--- sample_next_app/src/app/other/page.tsx
+++ sample_next_app/src/app/other/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
 import Image from "next/image"
-import './style.module.css';
+import styles from './style.module.css';
 
 export default function Other(){
     return (
         <main>
-            <h1 className="style.msg">Other page</h1>
-            <p className="style.title">이건 다른 페이지입니다.</p>
+            <h1 className={styles.msg}>Other page</h1>
+            <p className={styles.title}>이건 다른 페이지입니다.</p>
             <div>
                 <Image src="/sample.jpg" width={200} height={200} />
             </div>
