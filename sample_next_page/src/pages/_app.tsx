b4fecb05db438dc0339659204aecd226047d75f2
Hwangtaehun <r283@naver.com>
2025-02-21 19:41:47 +0900

    정적 페이지

--- sample_next_page/src/pages/_app.tsx
+++ sample_next_page/src/pages/_app.tsx
@@ -3,9 +3,6 @@ import type { AppProps } from 'next/app';
 import Layout from './_layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  console.log(pageProps);
+  return<Component{...pageProps} />;
 }
