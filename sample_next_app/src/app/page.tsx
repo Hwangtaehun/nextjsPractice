b8f84076e3b7aadec61b8091e4f68146596e171f
Hwangtaehun <r283@naver.com>
2025-02-22 21:13:46 +0900

    업로드

--- sample_next_app/src/app/page.tsx
+++ sample_next_app/src/app/page.tsx
@@ -1,35 +1,15 @@
-"use client";
-import JsonItem from './JsonItem';
-import useSWR from 'swr';
+"use server";
+import { SWRProvider } from "./swr-provider";
+import GetData from "./GetData";
 
-const url = '/sample.json';
-const fetcher = (...args) => 
-  fetch(...args).then(res => res.json()); 
-
-export default function Home() {
-  const {data, error, isLoading} = useSWR(url, fetcher);
-
-  const doItem = (value) => {
-    return <JsonItem data={value} />;
-  }
-
+export default async function Home() {
   return(
     <main>
       <h1 className="title">Index page</h1>
       <p className="msg font-bold">※SWR로 데이터를 가져옵니다.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>name</th>
-            <th>mail</th>
-            <th>age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data ? data.data.map((value)=>doItem(value))
-          : <tr><td>-</td><td>-</td><td>-</td></tr>}
-        </tbody>
-      </table>
+      <SWRProvider>
+        <GetData />
+      </SWRProvider>
     </main>
   );
 }
