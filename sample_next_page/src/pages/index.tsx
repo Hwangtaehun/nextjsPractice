11b8a3708d51ed14c5ed96c1d43a53436ca1f807
Hwangtaehun <r283@naver.com>
2025-02-22 21:52:45 +0900

    업로드

--- sample_next_page/src/pages/index.tsx
+++ sample_next_page/src/pages/index.tsx
@@ -1,22 +1,34 @@
 'use client';
-
 import { Inter } from 'next/font/google';
+import {useState} from 'react';
 import useSWR from 'swr';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const url = '/api/hello';
+const urlpath = '/api/data/';
 const fetcher = (...args) => fetch(...args)
   .then(res => res.json());
 
 export default function Home() {
+  const [num, setNum] = useState(0);
+  const [url, setUrl] = useState(urlpath + num);
   const {data, error, isLoading} = useSWR(url, fetcher);
+
+  const doChange = (event) => {
+    const val = event.target.value;
+    setNum(val);
+    setUrl(urlpath + val);
+  }
+
   return (
     <main>
       <h1 className="header">Index page</h1>
-      <p>API를 이용하는 예제입니다.</p>
+      <p>API 이용 예제입니다.</p>
+      <div>
+        <input type="number" min="0" max="3" onChange={doChange} value={num}/>
+      </div>
       <p className="border p-3">
-        result: {error ? "ERROR!!" : isLoading ? "loading..." : data.name}
+        result: {isLoading ? "reading..." : JSON.stringify(data)}
       </p>
     </main>
   );
