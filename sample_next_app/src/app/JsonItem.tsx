729ab1b9d5dc8853ca6f89c8ae88fe683b6a90e7
Hwangtaehun <r283@naver.com>
2025-02-22 21:11:34 +0900

    클라이언트 swr

---
+++ sample_next_app/src/app/JsonItem.tsx
@@ -0,0 +1,11 @@
+"use Client"
+
+export default function JsonItem(props) {
+    return(
+        <tr>
+            <td>{props.data.name}</td>
+            <td>{props.data.mail}</td>
+            <td>{props.data.age}</td>
+        </tr>
+    );
+}
--- sample_next_app/src/app/page.tsx
+++ sample_next_app/src/app/page.tsx
@@ -1,26 +1,35 @@
-"use client"
-import useSWR from "swr";
+"use client";
+import JsonItem from './JsonItem';
+import useSWR from 'swr';
 
 const url = '/sample.json';
-const fetcher = (...args) =>
-  fetch(...args).then(res => res.json());
+const fetcher = (...args) => 
+  fetch(...args).then(res => res.json()); 
 
-export default async function Home() {
+export default function Home() {
   const {data, error, isLoading} = useSWR(url, fetcher);
 
-  function doSWR() {
-    if (error) return <p>ERROR</p>;
-    if (isLoading) return <p>isLoading...</p>;
-    return <p>{data.message}</p>
+  const doItem = (value) => {
+    return <JsonItem data={value} />;
   }
 
-  return (
+  return(
     <main>
       <h1 className="title">Index page</h1>
-      <p className="msg font-bold">
-        ※SWR로 데이터를 가져옵니다.
-      </p>
-      <div className="border p-2 m-5">{doSWR()}</div>
+      <p className="msg font-bold">※SWR로 데이터를 가져옵니다.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>mail</th>
+            <th>age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data ? data.data.map((value)=>doItem(value))
+          : <tr><td>-</td><td>-</td><td>-</td></tr>}
+        </tbody>
+      </table>
     </main>
   );
 }
