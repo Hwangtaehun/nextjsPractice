e82158e5260dede973f96db619b0e76a0f5e37b4
Hwangtaehun <r283@naver.com>
2025-02-23 09:18:19 +0900

    저장

--- sample_next_app/src/app/rh/route.ts
+++ sample_next_app/src/app/rh/route.ts
@@ -1,8 +1,21 @@
 "use server";
 
+const url = 'http://localhost:3000/sample.json';
+
 export async function GET(request: Request){
-    const res = {content:'Hello, this is API content!'};
-    return new Response(JSON.stringify(res), {
+    const result = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await result.json();
+
+    const {searchParams} = new URL(request.url);
+    var id = +searchParams.get('id');
+    id = id < 0 ? 0 : id >= data.data.length ? data.data.length - 1 : id;
+    const item = data.data[id];
+
+    return new Response(JSON.stringify(item), {
         status: 200,
         headers: {'Content-Type': 'application/json'},
     });
