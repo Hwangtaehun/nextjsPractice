2f4289fb2149254c7c8ee0a4a42cdfc93c41db80
Hwangtaehun <r283@naver.com>
2025-02-23 10:00:02 +0900

    sumbit 예제
    api작성과 이용 완료

--- sample_next_app/src/app/page.tsx
+++ sample_next_app/src/app/page.tsx
@@ -6,8 +6,8 @@ const url = 'http://localhost:3000/rh';
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function Home() {
-  const [name,setName] = useState('')
-  const [pass,setPass] = useState('')
+  const [name,setName] = useState('');
+  const [pass,setPass] = useState('');
   const {data, error, mutate, isLoading} = useSWR(url, fetcher);
   const doName = (event) => {
     const val = event.target.value;
@@ -35,15 +35,23 @@ export default function Home() {
   return(
     <main>
       <h1 className="title">Login page</h1>
-      <p className="msg font-bold">※이름과 패스워드 입력:</p>
-      <form action={login}>
+      <p className="msg font-bold">
+        ※이름과 패스워드를 입력：</p>
+      <form onSubmit={(e) => { 
+        e.preventDefault(); 
+        login(new FormData(e.target)); 
+        }}>
       <div><input type="text" className="input mx-5 my-1"
         name="name" value={name} onChange={doName} /></div>
       <div><input type="password" className="input mx-5 my-1"
         name="pass" value={pass} onChange={doPass} /></div>
-      <div className="mx-3"><button className="btn my-1">Click</button></div>
+      <div className="mx-3"><button className="btn my-1">
+        Click</button></div>
       </form>
-      <pre className="msg border p-2">{error ? 'ERROR!!' : isLoading ? 'loading...' : data.content}</pre>
+      <pre className="msg border p-2">
+        {error ? 'ERROR!!' : isLoading ? 
+          'loading...' : data.content}
+      </pre>
     </main>
   );
 }
